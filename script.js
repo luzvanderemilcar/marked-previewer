24e2819b98793bbe7863e553dc84ff33de7b8d85
Luzvander EMILCAR <107006182+luzvanderemilcar@users.noreply.github.com>
2023-09-30 23:04:07 -0400

    Add files via upload
    Several changes in design :
    
    - Add meta tag for author, description and viewport
    - Add clear button to clear editor
    - Add placeholder text for editor 
    - Remove default text in editor
    
    - Change the view according to media query and viewport width. If it's a small screen, we can switch views between editor and preview. If it's a bigger screen,  you can only toggle preview.

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-
-const defaultInput = "# Text highlights\n\n____\n" +
-"## Bold\n\nStrong emphasis can be achieved by using the `<strong>` tag inside the HTML code. This change the text weight of the text inside the tags (opening and closing tags)." + "> *Example*\ > `<strong>Bolded Text</strong>`\n \t> Output: **Bolded Text**\n" + "[Text Highlight - More on MDN](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/strong) \n" +
-"1. To make stand out  2. To differ 3. To emphasize \n " + " 'span' \n " + " 	> Note :  \n" + "![lamp image](https://www.w3schools.com/images/lamp.jpg)\n" + " **Hint!**\nYou can use CSS property *text-weight* and set it's value to *bold* to achieve visual highlights with CSS." +
-"\`\`\` \n selector {\n text-weight: bold \n}\`\`\` \n";
-
 // Element setup
 const Editor = document.querySelector("#editor-div");
 const Preview = document.querySelector("#preview-div");
@@ -12,16 +6,16 @@ const previewBody = document.querySelector("#preview.card-body");
 const ViewController = document.querySelector("#view-controller");
 const Eraser = document.querySelector("#eraser");
 
-editorField.innerHTML = defaultInput;
-previewBody.innerHTML = DOMPurify.sanitize(marked.parse(editorField.innerHTML));
+let smallScreen;
 
+// Event Listeners
 editorField.addEventListener("input", updateValue);
 
 ViewController.addEventListener("click", toggleView);
 
 Eraser.addEventListener("click", ()=> {
-  editorField.innerHTML = "";
-  previewBody.innerHTML = ""
+  previewBody.innerHTML = "";
+  editorField.value = "";
 });
 
 function updateValue(e) {
@@ -30,13 +24,39 @@ function updateValue(e) {
 
 // switch view between editor and preview 
 function toggleView() {
-  if (Preview.style.display == "none") {
+  if (smallScreen) {
+  if (Editor.style.display == "none") {
+    Preview.style.display = "none"
+    Editor.style.display = "block";
+    ViewController.innerHTML = '<i class="fa-regular fa-eye"></i>';
+  } else {
 Preview.style.display = "block";
 Editor.style.display = "none";
 ViewController.innerHTML = '<i class="fa-regular fa-pen-to-square"></i>';
+  }
   } else {
-Preview.style.display = "none"
-Editor.style.display = "block";
-ViewController.innerHTML = '<i class="fa-regular fa-eye"></i>';
+    if (Editor.style.display == "none" ) {
+      
+    }
+    if (Preview.style.display == "block")  {
+       Preview.style.display = "none";
+       ViewController.innerHTML = '<i class="fa-regular fa-eye"></i>';
+     } else {
+       Preview.style.display = "block";
+       ViewController.innerHTML = '<i class="fa-regular fa-eye-slash"></i>';
+     }
   }
 }
+
+// Check the size of screen base on media query
+function screenWidthStateLever(media){
+  if (media.matches) {
+    smallScreen = true;
+  } else {
+    smallScreen = false;
+  }
+}
+const smallScreenMedia= window.matchMedia("(max-width: 500px)");
+
+screenWidthStateLever(smallScreenMedia);
+smallScreenMedia.addListener(screenWidthStateLever);
